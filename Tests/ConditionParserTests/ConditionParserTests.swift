f703064258005db2387f7ea8f26e63cf7a36616e
Jacopo Mangiavacchi <Jacopo.Mangiavacchi@uk.ibm.com>
2017-03-07 15:25:32 +0000

    refactored test

--- Tests/ConditionParserTests/ConditionParserTests.swift
+++ Tests/ConditionParserTests/ConditionParserTests.swift
@@ -1,25 +1,33 @@
 import XCTest
-@testable import ConditionParser
+@testable import SwiftExpressionLanguage
 
 class ConditionParserTests: XCTestCase {
     func testExample1() {
-        XCTAssertEqual(try "(1 == 2 || 2 < 4) && 'test' != 'ko'".checkCondition(), true)
+        let condition = "(1 == 2 || 2 < 4) && 'test' != 'ko'"
+        let exp = SwiftExpressionLanguage(condition)
+
+        XCTAssertEqual(try exp.checkCondition(), true)
     }
 
     func testExample2() {
-        XCTAssertEqual(try "(var1 == var2 || var2 < var3) && var4 != var5".checkCondition(withVariables: [
-            "var1" : 1,
-            "var2" : 2,
-            "var3" : 4,
-            "var4" : "test",
-            "var5" : "ko"
-            ]), true)
+        XCTAssertEqual(try "(1 == 2 || 2 < 4) && 'test' != 'ko'".checkCondition(), true)
+    }
+
+    func testExample3() {
+         XCTAssertEqual(try "(var1 == var2 || var2 < var3) && var4 != var5".checkCondition(withVariables: [
+             "var1" : 1,
+             "var2" : 2,
+             "var3" : 4,
+             "var4" : "test",
+             "var5" : "ko"
+             ]), true)
     }
 
     static var allTests : [(String, (ConditionParserTests) -> () throws -> Void)] {
         return [
             ("testExample1", testExample1),
-            ("testExample2", testExample2)
+            ("testExample2", testExample2),
+            ("testExample3", testExample3)
         ]
     }
 }
